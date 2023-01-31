46d27d89d944640d46719c7dea168c0d81e19d2f
ThomasCarney315 <thomascarney315@gmail.com>
2023-01-31 01:16:25 -0800

    Added Data Validation

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -1,16 +1,28 @@
 print('Hello, my name is Alex your virtual assistant. I will help you order a pizza!')
 print('I will ask you a few questions. After typing an answer, press enter.')
 userName = input('\nEnter your name:  ')
+while len(userName) == 0:
+    userName = input('You must have a name! To continue, please enter your name:  ')
 if userName.lower() == 'thomas':
     print(f'\n Greetings {userName} The Creator, my actions are at your fingertips. What is your will?')
 else:
     print(f'\nHello, {userName}. Nice to meet you!')
 size = input('\nWhat size pizza would you like? Enter small, medium, or large:  ')
+while size.lower() not in ['small', 'medium', 'large']:
+    size = input('\nPlease choose small, medium, or large:  ')
 flavor = input('\nEnter the flavor of pizza you want:  ')
+while len(flavor) == 0:
+    flavor = input('\nDon\'t you like flavor? Please pick a flavor:  ')
 crustType = input('\n What type of crust would you like:  ')
+while len(crustType) == 0:
+    crustType = input('\nPlease pick a crust type:  ')
 quantity = input('\nHow many of these do you want to order? Enter a numeric value:  ')
+while not quantity.isdigit():
+    quantity = input('\nThat\'s not a number. Please enter a number:  ')
 quantity = int(quantity)
 method = input('\nIs this for carry out or delivery:  ')
+while method.lower() not in ['carry out', 'delivery']:
+    method = input('\nPlease enter carry out or delivery:  ')
 salesTax = 1.1
 if size.lower() == 'small':
     pizzaCost = 8.99
