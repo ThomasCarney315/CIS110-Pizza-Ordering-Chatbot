631416a8fd8f3a353a8c6ac7d0ef640215d85a90
ThomasCarney315 <thomascarney315@gmail.com>
2023-02-07 04:48:07 -0800

    added iteration constructs

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -7,39 +7,53 @@ if userName.lower() == 'thomas':
     print(f'\n Greetings {userName} The Creator, my actions are at your fingertips. What is your will?')
 else:
     print(f'\nHello, {userName}. Nice to meet you!')
-size = input('\nWhat size pizza would you like? Enter small, medium, or large:  ')
-while size.lower() not in ['small', 'medium', 'large']:
-    size = input('\nPlease choose small, medium, or large:  ')
-flavor = input('\nEnter the flavor of pizza you want:  ')
-while len(flavor) == 0:
-    flavor = input('\nDon\'t you like flavor? Please pick a flavor:  ')
-crustType = input('\n What type of crust would you like:  ')
-while len(crustType) == 0:
-    crustType = input('\nPlease pick a crust type:  ')
-quantity = input('\nHow many of these do you want to order? Enter a numeric value:  ')
-while not quantity.isdigit():
-    quantity = input('\nThat\'s not a number. Please enter a number:  ')
-quantity = int(quantity)
-method = input('\nIs this for carry out or delivery:  ')
-while method.lower() not in ['carry out', 'delivery']:
-    method = input('\nPlease enter carry out or delivery:  ')
-salesTax = 1.1
-if size.lower() == 'small':
-    pizzaCost = 8.99
-elif size.lower() == 'medium':
-    pizzaCost = 14.99
-elif size.lower() == 'large':
-    pizzaCost = 17.99
-if method.lower() == 'delivery':
-    deliveryFee = 5
-else:
-    deliveryFee = 0
-total = (pizzaCost * quantity) * salesTax + deliveryFee
-print('-' * 10)
-print(f'Thank you, {userName}, for your order.')
-print(f'Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:,.2f}.')
-if total >= 50:
-    print('\nCongrats! You get 10$ off your next order!')
-else:
-    print('\nAny order of 50$ or more will reward you with 10$ off your next order.')
-print('-' * 10)
+
+keepGoing = 'y'
+while keepGoing.lower() == 'y':
+
+    size = input('\nWhat size pizza would you like? Enter small, medium, or large:  ')
+    while size.lower() not in ['small', 'medium', 'large']:
+        size = input('\nPlease choose small, medium, or large:  ')
+    flavor = input('\nEnter the flavor of pizza you want:  ')
+    while len(flavor) == 0:
+        flavor = input('\nDon\'t you like flavor? Please pick a flavor:  ')
+    crustType = input('\n What type of crust would you like:  ')
+    while len(crustType) == 0:
+        crustType = input('\nPlease pick a crust type:  ')
+    quantity = input('\nHow many of these do you want to order? Enter a numeric value:  ')
+    while not quantity.isdigit():
+        quantity = input('\nThat\'s not a number. Please enter a number:  ')
+    quantity = int(quantity)
+    method = input('\nIs this for carry out or delivery:  ')
+    while method.lower() not in ['carry out', 'delivery']:
+        method = input('\nPlease enter carry out or delivery:  ')
+    salesTax = 1.1
+    if size.lower() == 'small':
+        pizzaCost = 8.99
+    elif size.lower() == 'medium':
+        pizzaCost = 14.99
+    elif size.lower() == 'large':
+        pizzaCost = 17.99
+    if method.lower() == 'delivery':
+        deliveryFee = 5
+    else:
+        deliveryFee = 0
+    total = (pizzaCost * quantity) * salesTax + deliveryFee
+    print('-' * 10)
+    print(f'Thank you, {userName}, for your order.')
+    print(f'Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:,.2f}.')
+    if total >= 50:
+        print('\nCongrats! You get 10$ off your next order!')
+    else:
+        print('\nAny order of 50$ or more will reward you with 10$ off your next order.')
+    print('-' * 10)
+    print('\nYour order has been recieved. ETA 3 mins!')
+    for min in range(3, 0, -1):
+        print(min, 'minutes remaining')
+        for seconds in range(60, 0, -1):
+            print(seconds, end= ' \r')
+            import time
+            time.sleep(1)
+    print('Order is ready!')
+
+    keepGoing = input('/nWould you like to place another order? Enter y or n:  ')
